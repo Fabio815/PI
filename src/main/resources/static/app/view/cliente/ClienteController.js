ad4d42c82ff72eeeec19a70ad8f465451d3d8a63
Fabio815 <fabiomotamarques815@gmail.com>
2026-04-13 22:52:15 -0300

    mudando o end-point do adicionar cliente

--- src/main/resources/static/app/view/cliente/ClienteController.js
+++ src/main/resources/static/app/view/cliente/ClienteController.js
@@ -1,6 +1,6 @@
 Ext.define('ProjSistemaOs.view.cliente.ClienteController', {
 	extend: 'Ext.app.ViewController',
-    alias: 'controller.cliente-controller',
+	alias: 'controller.cliente-controller',
 
 	requires: [
 		'ProjSistemaOs.util.MensagemUtil'
@@ -14,24 +14,20 @@ Ext.define('ProjSistemaOs.view.cliente.ClienteController', {
 	},
 	salvarCliente: function() {
 		var me = this, vw = me.getView(),
-		form = vw.down('form').getForm().getValues();
-
-		console.log(form);
+			form = vw.down('form').getForm().getValues();
 		Ext.Ajax.request({
-			url: 'http://localhost:8080/cliente/cadastrar',
+			url: 'http://localhost:8080/sistema-os/api/cliente/cadastrar',
 			method: 'POST',
 			jsonData: form,
 			callback: function(options, success, response) {
-				debugger;
-
 				if (vw && !vw.destroyed && !vw.isDestroying) {
 					var r = Ext.decode(response.responseText, true);
 					if (r && r.sucesso) {
 						vw.fireEvent('clientesalvo'); //Dispara o evento quando salva o cliente.
-					 	Avisos.mensagemSucesso(r.mensagem);
+						Avisos.mensagemSucesso(r.mensagem);
 						vw.close();
 					} else if (r) {
-						 Avisos.mensagemAviso(r.mensagem);
+						Avisos.mensagemAviso(r.mensagem);
 					} else {
 						Avisos.mostrarServidorIndisponivel();
 					}
