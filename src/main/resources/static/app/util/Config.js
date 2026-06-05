ee117ba286548a78f98d845bb4c1d094094b1018
Fabio815 <fabiomotamarques815@gmail.com>
2026-06-05 09:52:55 -0300

    mudando url

--- src/main/resources/static/app/util/Config.js
+++ src/main/resources/static/app/util/Config.js
@@ -2,5 +2,6 @@ Ext.define('ProjSistemaOs.util.Config', {
     alternateClassName: 'sistemaOsLocal',
     singleton: true,
 
-    apiUrl: 'http://localhost:8080'
+    //apiUrl: 'http://localhost:8080'
+    apiUrl: window.location.origin
 });
