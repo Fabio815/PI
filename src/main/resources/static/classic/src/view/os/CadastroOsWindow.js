cda7102d09275a5c8c50421ab56107adfcf2e67a
Fabio815 <fabiomotamarques815@gmail.com>
2026-06-20 18:01:02 -0300

    Finalizado o window dw cdastro de OS

--- src/main/resources/static/classic/src/view/os/CadastroOsWindow.js
+++ src/main/resources/static/classic/src/view/os/CadastroOsWindow.js
@@ -81,6 +81,60 @@ Ext.define('ProjSistemaOs.view.os.CadastroOsWindow', {
             fieldLabel: 'Mão de obra',
             margin: '0 0 0 10'
         }]
+    }, {
+        xtype: 'grid',
+        padding: 10,
+        reference: 'gridPecas',
+        title: 'Peças',
+        height: 200,
+        ui: 'light',
+        shadow: true,
+        store: {
+            fields: [
+                'id',
+                'descricao',
+                'quantidade',
+                'valorUnitario'
+            ],
+            data: [
+                { id: 1, descricao: 'Corrente', quantidade: 1, valorUnitario: 250.00 },
+                { id: 2, descricao: 'Pneu', quantidade: 2, valorUnitario: 89.90 },
+                { id: 3, descricao: 'Aro 29', quantidade: 1, valorUnitario: 45.00 },
+                { id: 4, descricao: 'Marcha', quantidade: 1, valorUnitario: 30.00 }
+            ]
+        },
+
+        columns: [{
+            text: 'Código',
+            dataIndex: 'id',
+            flex: 1,
+        }, {
+            text: 'Peça',
+            dataIndex: 'descricao',
+            flex: 5
+        }, {
+            text: 'Qtd',
+            dataIndex: 'quantidade',
+            flex: 1
+        }, {
+            text: 'Valor unitário',
+            dataIndex: 'valorUnitario',
+            flex: 2
+        }, {
+            xtype: 'actioncolumn',
+            width: 80,
+            text: 'Remover',
+            items: [{
+                iconCls: 'fa fa-times',
+                tooltip: 'Remover',
+                handler: function (grid, rowIndex) {
+                    grid.getStore().removeAt(rowIndex);
+                }
+            }]
+        }]
+    }, {
+        xtype: 'textarea',
+        fieldLabel: 'Observações',
     }],
 
     buttons: [{
