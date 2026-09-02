25d1aebd37be0b3888fdf77e1152ce335d0da8c2
Gabrielle-Duarte <gabrielle_hbo@hotmail.com>
2026-09-02 16:19:53 -0300

    Criação do criar OS

--- src/main/java/br/com/sistemaos/infraestrura/dto/OsDTO.java
+++ src/main/java/br/com/sistemaos/infraestrura/dto/OsDTO.java
@@ -1,4 +1,27 @@
 package br.com.sistemaos.infraestrura.dto;
 
+import br.com.sistemaos.domain.entity.Cliente;
+import br.com.sistemaos.domain.entity.Os;
+import br.com.sistemaos.domain.entity.Usuario;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class OsDTO {
+    private Usuario usuario;
+    private Cliente cliente;
+    private String status;
+
+    public static OsDTO criar (Os os){
+
+        return OsDTO.builder()
+                .id(os.getId())
+                .idCliente(os.getCliente().getId())
+                .idUsuario(os.getUsuario().getId())
+                .status(os.getStatus().name())
+                .build();
+    }
 }
